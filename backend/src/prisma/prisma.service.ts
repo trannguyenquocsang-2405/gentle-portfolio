a8e4cf2b0b8363a1d8a57024bb8e9a7636d6a386
trannguyenquocsang-2405 <trannguyenquocsang@gmail.com>
2026-06-22 14:40:12 +0700

    Fix prisma

--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,11 +3,6 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-  constructor() {
-    super({
-      datasourceUrl: process.env.DATABASE_URL,
-    });
-  }
 
   async onModuleInit() {
     await this.$connect();
